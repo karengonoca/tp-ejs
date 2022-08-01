226f6f2406bfd051ed4d2c3c7cacabf17d9c1fca
Karen Gonzalez <kagoncaoca@gmail.com>
2022-08-01 20:19:28 -0300

    tp-created

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const {detail} = require('../controllers/productsController')
+
+
+/* /products */
+router
+    .get('/detail/:id', detail)
+
+
+
+module.exports = router;
